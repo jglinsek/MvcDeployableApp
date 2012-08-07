d7444597d32338ca12767e44903c503e4aa5c01a
James G <james.glinsek@gmail.com>
2012-08-07 14:22:54 -0500

    Added octopus stuff back in.

---
+++ src/Deploy.ps1
@@ -0,0 +1,4 @@
+﻿﻿# This PowerShell script will be automatically called by Octopus when the package is installed on a remote machine. 
+# More details about this feature can be found here: http://octopusdeploy.com/documentation/features/powershell
+
+Write-Host "Hello from PowerShell!"
